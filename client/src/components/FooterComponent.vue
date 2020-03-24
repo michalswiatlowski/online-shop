f0f5508470cd58dad8b966611d88ceea40af584e
michalswiatlowski <kontakt@michalswiatlowski.pl>
2020-03-24 22:26:32 +0100

    add styling to footer component

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -1,17 +1,41 @@
 <template>
-  <footer>
-    <div>
-      Funemki &copy; 2020 
-    </div>
+  <footer class="footer">
+    <a href="#" class="paragraph">Polityka prywatności</a>
+    <p class="paragraph">&copy; {{ currentYear }} Funemki. All rights reserved</p>
   </footer>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      currentYear: (new Date()).getFullYear(),
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.footer {
+text-align: center;
+padding: 10px;
+}
 
+.paragraph {
+  font-size: 0.6rem;
+  color: var(--lightgrey);
+  transition: 0.2s;
+}
+
+a.paragraph:hover {
+  color: var(--grey);
+  transition: 0.2s
+}
+
+@media (min-width: 768px)  {
+  .footer {
+    padding-left: 100px;
+    text-align: left;
+  }
+}
 </style>
