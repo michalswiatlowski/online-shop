2e734f63fc47abe88f5289032e6c911de899bd35
michalswiatlowski <kontakt@michalswiatlowski.pl>
2020-06-26 22:24:57 +0200

    add vuetify to project

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import { routes } from './router/routes.js';
+import vuetify from './plugins/vuetify';
 
 Vue.use(VueRouter);
 
@@ -22,5 +23,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App),
+  vuetify,
+  render: (h) => h(App)
 }).$mount('#app');
--- client/vue.config.js
+++ client/vue.config.js
@@ -9,4 +9,7 @@ module.exports = {
       },
     },
   },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 };
